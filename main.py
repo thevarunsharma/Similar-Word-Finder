f71a89e6cf0015d2aa2f9918fd7e6a83f03d1b15
Beast <varunsharma123.del@gmail.com>
2018-10-06 11:31:19 +0530

    main files

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import mlcode as ml
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def main():
+    if request.method=="POST":
+        data = request.form
+        word = data['word'].lower()
+        sim = ml.get_similar_words(word)
+        found = True
+        if sim=="Word not in vocabulary!":
+            found = False
+        return render_template('main.html', after_srch=True, found=found, word=word, results=sim)
+    return render_template('main.html', after_srch=False)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.run(debug=True)
---
+++ mlcode.py
@@ -0,0 +1,48 @@
+import numpy as np
+from pickle import load
+
+with open('words.dat', 'rb') as f:
+    words = load(f)
+
+vecs = np.load('vecs.npy')
+
+def similarity(u, v):
+    num = np.dot(u, v.T).reshape((u.shape[0], 1))
+    den = (np.linalg.norm(u, axis=1)*np.linalg.norm(v, axis=1)).reshape((u.shape[0], 1))
+    sim = num/den
+    return sim.ravel()
+
+def search(wrd, arr):       #binary search
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid]==wrd:
+            return mid
+        if arr[mid] < wrd:
+            low = mid+1
+        if arr[mid] > wrd:
+            high = mid-1
+    return -1
+
+def find_similar(word, words=words, vecs=vecs):
+    idx = search(word, words)
+    if idx==-1:
+        return "Word not in vocabulary!"
+    vec1 = vecs[idx].reshape((1,50))
+    sims = similarity(vecs, vec1)
+    simidx = np.where(sims > 0.7)[0]
+    simwords = []
+    for i in simidx:
+        if i!=idx:
+            simwords.append((words[i], sims[i]))
+    return simwords
+
+def filter_results(simwords):
+    if simwords=="Word not in vocabulary!":
+        return simwords
+    simwords = sorted(simwords, key = lambda x:x[1], reverse=True)
+    return list(np.array(simwords)[:5,0])
+
+def get_similar_words(word):    #wrapper function
+    return filter_results(find_similar(word))
